adc3c08dca8ed81325532c36b1dfed5673f9d91c
LeeRatek <sm.lee@newratek.com>
2024-01-30 11:58:48 +0900

    Revised at 2024-01-30 11:58

--- scratch/triggered-txs-mode-1-example.cc
+++ scratch/triggered-txs-mode-1-example.cc
@@ -59,29 +59,18 @@
 #include "ns3/ssid.h"
 #include "ns3/stats-module.h"
 #include "ns3/string.h"
-<<<<<<< HEAD
 #include "ns3/txs-wifi-mac-helper.h"
-=======
->>>>>>> 55d1b4469291f0faad2ffccdc1474b85f82248b3
 #include "ns3/udp-client-server-helper.h"
 #include "ns3/uinteger.h"
 #include "ns3/wifi-acknowledgment.h"
 #include "ns3/wifi-mac-queue.h"
-<<<<<<< HEAD
 #include "ns3/wifi-mac.h"
-=======
-
->>>>>>> 55d1b4469291f0faad2ffccdc1474b85f82248b3
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("triggered-txs-mode-1-example");
 
 void
-<<<<<<< HEAD
 SpectrumNetworkInstallation(TxsWifiMacHelper& mac,
-=======
-SpectrumNetworkInstallation(WifiMacHelper& mac,
->>>>>>> 55d1b4469291f0faad2ffccdc1474b85f82248b3
                             SpectrumWifiPhyHelper& phy,
                             Ssid& ssid,
                             WifiHelper& wifi,
@@ -182,12 +171,8 @@ main(int argc, char* argv[])
     double distance{1.0}; // meters
 
     // Network setting
-<<<<<<< HEAD
     bool isEhtNetwork{true}; // True: EHT network, False: HE network
     bool txsEnabled{true};   // True: TXS supported
-=======
-    bool isEhtNetwork{true}; // True: TXS, False: HE network
->>>>>>> 55d1b4469291f0faad2ffccdc1474b85f82248b3
     bool udp{true};
     bool enableUlOfdma{true}; // True: mu-ul
     bool enableBsrp{false};
@@ -250,15 +235,11 @@ main(int argc, char* argv[])
             NetDeviceContainer apDevice;
             NetDeviceContainer staDevices;
 
-<<<<<<< HEAD
             txsEnabled = (isEhtNetwork && txsEnabled)
                              ? txsEnabled
                              : false; // TXS supported is enabled in EHT network
             TxsWifiMacHelper mac(txsEnabled);
 
-=======
-            WifiMacHelper mac;
->>>>>>> 55d1b4469291f0faad2ffccdc1474b85f82248b3
             WifiHelper wifi;
             std::string channelStr("{" + std::to_string(channelIndex) + ", " +
                                    std::to_string(channelWidth) + ", ");
